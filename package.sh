3fe8b77c4dc899ac5bb0a78cbfe3b1dce2683f91
Decoy <63180896+Faggio-Java@users.noreply.github.com>
2022-02-09 18:50:51 -0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,15 +1,8 @@
 #!bin/bash
 
-port="ports/$1/wget.sh"
-
-if [ -f $port ]
-then
-  sh $port && tar -xf $1.tar.xz && sh $1/install.sh
-else
-   if [ "$1" == "installed"  ]
-   then
-      echo -e "$(<ports/installed.sh)"
-   else
-      echo "Error: $1 couldn't be found"
-   fi
+if wget -q --method=HEAD https://sourceforge.net/projects/bin-lily/files/$1.tar.xz;
+ then
+  wget https://sourceforge.net/projects/bin-lily/files/$1.tar.xz
+ else
+  echo "Error: couldn't find the package '$1'"
 fi
