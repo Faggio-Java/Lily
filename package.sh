a02febde7541b7b88598e3107437d602dc52beef
Decoy <63180896+Faggio-Java@users.noreply.github.com>
2022-02-09 19:31:08 -0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -3,6 +3,8 @@
 if wget -q --method=HEAD https://sourceforge.net/projects/bin-lily/files/$1.tar.xz;
  then
   wget https://sourceforge.net/projects/bin-lily/files/$1.tar.xz
+  tar -xf $1.tar.xz
+  sh $1/install.sh
  else
   echo "Error: couldn't find the package '$1'"
 fi
