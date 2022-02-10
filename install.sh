536f4712a2b8d430dc1a661a7d8c4279f776923c
Decoy <63180896+Faggio-Java@users.noreply.github.com>
2022-02-09 21:34:46 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ sudo mkdir /usr/lily
 sudo mv ports/ /usr/lily/
 sudo mv lily /usr/lily/
 sudo chown -r $(whoami) /usr/lily 
-sudo ln -s /usr/bin/lily/lily /usr/bin/
+sudo echo "alias lily=/usr/lily/package.sh" >> ~/.bashrc && . ~/.bashrc
